07a3187b9e570657b5d19984cc8bc8e143235150
Arimateia Morais <arimateiamf@gmail.com>
2019-03-22 21:01:47 -0300

    [WIP]

--- src/parsers/Codeforces.ts
+++ src/parsers/Codeforces.ts
@@ -6,28 +6,29 @@ import fs from 'fs';
 export class Codeforces implements Parser{
   urlBase = 'https://codeforces.com';
 
-  public parseProblem(idProblem: string):void {
+  public async parseProblem(idProblem: string):Promise<void> {
     const urlAux: string = idProblem.replace(/-/g, '/');
     const urlProblemSet: string = '/problemset/problem';
     const finalUrl: string = this.urlBase + urlProblemSet + urlAux;
-    const data = this.parseQuestion(finalUrl);
+    const data = await this.getTestsQuestion(finalUrl);
+    console.log(data);
     const dataJson = JSON.stringify(data, null, 2);
     fs.writeFileSync('./data', dataJson);
   }
 
-  async public parseContest(idContest: string):void {
+  public parseContest(idContest: string):void {
     const urlAux: string = idContest.replace(/-/g, '/');
     const urlContest: string = '/contest';
     const finalUrl: string = this.urlBase + urlContest + urlAux;
-    console.log(finalUrl);
     const data = [];
-    request(finalUrl, (error, response, html) => {
+    request(finalUrl,  (error, response, html) => {
       if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html);
         $('td.id').each((i, element) => {
-          const urlProblem: string = $(element).find('a').attr('href');
-          console.log(this.urlBase + urlProblem);
-          data[i] = await this.getTestsQuestion(this.urlBase + urlProblem);
+          const urlProblem: string =  $(element).find('a').attr('href');
+          data[i] = this.getTestsQuestion(this.urlBase + urlProblem);
+          console.log(data[i]);
+          console.log(i);
         });
       }
     });
@@ -35,9 +36,8 @@ export class Codeforces implements Parser{
     fs.writeFileSync('./data', dataJson);
   }
 
-  public getTestsQuestion(url: string): Promise {
-    console.log('aqui');
-    return new Promise(request(url, (error, response, html) => {
+  public getTestsQuestion(url: string): Promise<any[]> {
+    return new Promise(resolve => request(url, (error, response, html) => {
       const data = [];
       if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html);
@@ -46,10 +46,11 @@ export class Codeforces implements Parser{
             input: $(element).find('pre').text(),
             output: $(element).next().find('pre').text(),
           };
+          console.log(data[i]);
         });
       }
-      return data;
-    }); )
+      resolve(data);
+    }));
 
   }
 
