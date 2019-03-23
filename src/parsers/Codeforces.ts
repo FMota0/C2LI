914820e0b0a1cea7ad0297f91008aced3869ca45
Arimateia Morais <arimateiamf@gmail.com>
2019-03-23 01:21:43 -0300

    trying to end contest parser

--- src/parsers/Codeforces.ts
+++ src/parsers/Codeforces.ts
@@ -10,34 +10,23 @@ export class Codeforces implements Parser{
     const urlAux: string = idProblem.replace(/-/g, '/');
     const urlProblemSet: string = '/problemset/problem';
     const finalUrl: string = this.urlBase + urlProblemSet + urlAux;
-    const data = await this.getTestsQuestion(finalUrl);
+    const data = await this.getTestsProblem(finalUrl);
     console.log(data);
     const dataJson = JSON.stringify(data, null, 2);
     fs.writeFileSync('./data', dataJson);
   }
 
-  public parseContest(idContest: string):void {
+  public async parseContest(idContest: string): Promise<void> {
     const urlAux: string = idContest.replace(/-/g, '/');
     const urlContest: string = '/contest';
     const finalUrl: string = this.urlBase + urlContest + urlAux;
-    const data = [];
-    request(finalUrl,  (error, response, html) => {
-      if (!error && response.statusCode === 200) {
-        const $ = cheerio.load(html);
-        $('td.id').each((i, element) => {
-          const urlProblem: string =  $(element).find('a').attr('href');
-          data[i] = this.getTestsQuestion(this.urlBase + urlProblem);
-          console.log(data[i]);
-          console.log(i);
-        });
-      }
-    });
+    const data = await this.getContestProblems(finalUrl);
     const dataJson = JSON.stringify(data, null, 2);
     fs.writeFileSync('./data', dataJson);
   }
 
-  public getTestsQuestion(url: string): Promise<any[]> {
-    return new Promise(resolve => request(url, (error, response, html) => {
+  public getTestsProblem(url: string): Promise<any[]> {
+    return new Promise((resolve, reject) => request(url, (error, response, html) => {
       const data = [];
       if (!error && response.statusCode === 200) {
         const $ = cheerio.load(html);
@@ -46,12 +35,38 @@ export class Codeforces implements Parser{
             input: $(element).find('pre').text(),
             output: $(element).next().find('pre').text(),
           };
-          console.log(data[i]);
         });
+        resolve(data);
+
+      } else {
+        console.log('deu errado');
+        reject();
       }
-      resolve(data);
     }));
 
+  }
+
+  public async getContestProblems(url: string): Promise<any[]> {
+    return new Promise((resolve, reject) => request(url,async (error, response, html) => {
+      console.log('to aqui');
+      const data = [];
+      if (!error && response.statusCode === 200) {
+        console.log('vo pegar o parser');
+        const $ = cheerio.load(html);
+        const urls = [];
+        $('td.id').each(async(i, element) =>  {
+          const urlProblem: string =  $(element).find('a').attr('href');
+          console.log(urlProblem);
+          console.log('peguei url');
+          data[i] = await this.getTestsProblem(this.urlBase + urlProblem);
+          console.log(data[i]);
+        });
+        resolve(data);
+      } else {
+        console.log('deu errado');
+        reject();
+      }
+    }));
   }
 
 }
