3857c01614f33491cda53fe0628831a0ff08ca2c
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-04-03 16:33:17 -0300

    Update test writer

--- src/TestWriter.ts
+++ src/TestWriter.ts
@@ -3,12 +3,27 @@ import fs from 'fs'
 class TestWriter {
 
   public writeContestTests (tags: string[], contestTests: ContestTests) {
-    console.log(tags, contestTests)
+    if (!fs.existsSync(tags[1])) {
+      fs.mkdirSync(tags[1])
+    }
+
+    const problems = Object.keys(contestTests)
+    problems.forEach(problem => this.writeProblemTests(['', '', problem], contestTests[problem], tags[1]))
   }
 
-  public writeProblemTests (tags: string[], problemTests: ProblemTests) {
-    // fs.writeFileSync(`./A/${problemId}.json`, JSON.stringify(problemTests))
-    console.log(tags, problemTests)
+  public writeProblemTests (tags: string[], problemTests: ProblemTests, directory?: string) {
+    let testsDirectory = null
+    if (directory) {
+      testsDirectory = `./${directory}/${tags[2]}`
+    } else {
+      testsDirectory = `./${tags[1] + tags[2]}`
+    }
+
+    if (!fs.existsSync(testsDirectory)) {
+      fs.mkdirSync(testsDirectory)
+    }
+
+    fs.writeFileSync(testsDirectory + `/.tests.json`, JSON.stringify(problemTests))
   }
 }
 
