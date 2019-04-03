efb498661ec500be200caa4a910b1ff547ba26bb
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-04-03 15:46:20 -0300

    Add global types definitions

---
+++ typings/global.d.ts
@@ -0,0 +1,16 @@
+interface ProblemTest {
+  input: string
+  output: string
+}
+
+type ProblemTests = ProblemTest[]
+
+type ProblemId = string
+
+type ContestTests = Record<ProblemId, ProblemTests>
+
+interface Parser {
+  urlBase: string;
+  parseProblem: (idProblem: string) => Promise<ProblemTests>;
+  parseContest: (idContest: string) => Promise<ContestTests>;
+}
