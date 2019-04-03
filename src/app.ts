49cbf08dcca2bc1bfb35fd02fca238eebfe8dbe0
codefactor-io <support@codefactor.io>
2019-04-03 09:58:20 +0000

    [CodeFactor] Apply fixes to commit 17acecf

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import yargs from 'yargs';
+import yargs from "yargs";
 
 yargs
-.usage('Usage: cli-crud <command> [options]')
-.commandDir('cmds', {
-  extensions:['ts', 'js']
+.usage("Usage: cli-crud <command> [options]")
+.commandDir("cmds", {
+  extensions: ["ts", "js"],
 })
 .strict()
-.demandCommand(1, 'You need to run a command')
+.demandCommand(1, "You need to run a command")
 .help()
 .argv;
