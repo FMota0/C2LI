84df21931122c62fc0c5d653780a57dd59e1a494
Arimateia Morais <arimateiamf@gmail.com>
2019-04-05 23:18:21 -0300

    Organizing the code

--- src/cmds/add.ts
+++ src/cmds/add.ts
@@ -1,12 +1,22 @@
-import Manager from '../Manager';
 import inquirer from 'inquirer';
-export const command:string = ('add');
-export const desc:string = ('add tests for a specific problem');
-export const handler = (argv:any) => {
+
+import Manager from '../Manager';
+
+export const command:string = 'add';
+export const desc:string = 'add tests for a specific problem';
+export const handler = () => {
   const manager = new Manager();
   const question = [
-    { type: 'input' , name : 'input', message:'Type the input of the new tests' },
-    { type:'input', name:'output', message:'type the expected output of the new tests' },
+    {
+      type: 'input',
+      name : 'input',
+      message:'Type the input of the new tests',
+    },
+    {
+      type:'input',
+      name:'output',
+      message:'type the expected output of the new tests',
+    },
   ];
   inquirer
           .prompt(question)
