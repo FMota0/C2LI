f9c32e6e8a001552d124263f60acdb1782b76f1d
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-04-06 21:23:26 -0300

    Add atcoder parsing for contest

--- src/parsers/AtCoder.ts
+++ src/parsers/AtCoder.ts
@@ -12,14 +12,36 @@ class AtCoder implements Parser {
     const response = await fetch(this.buildContestUrl(contestId));
     const html = await response.text();
     const $ = cheerio.load(html);
-    $('tbody tr').each((i, element) => {
-      
+    const problemIds: string[] = [];
+    $('tbody tr td a').each((i, element) => {
+      if(i%2 == 0) {
+        const url = $(element).attr('href');
+        const problemId = url.split('/').slice(-1)[0];
+        problemIds.push(problemId);
+      }
     });
-    return Promise.resolve({});
+    const problemsPromises = problemIds.map((problemId) => {
+      return new Promise<[ProblemTests, ProblemId]>(
+        (resolve, reject) => {
+          this
+          .parseProblem(problemId, contestId)
+          .then(
+            (problemTests: ProblemTests) => resolve([problemTests, problemId]),
+          )
+          .catch(
+            reject,
+          );
+        },
+      )
+    });
+    const contestTests: ContestTests = {};
+    const promisesResults: [ProblemTests, ProblemId][] = await Promise.all(problemsPromises);
+    promisesResults.forEach(result => contestTests[result[1]] = result[0])
+    return contestTests;
   }
 
   public buildContestUrl(contestId: string): string {
-    return `${this.baseUrl}/${contestId}/tasks`;
+    return `${this.baseUrl}/contests/${contestId}/tasks`;
   }
 }
 
