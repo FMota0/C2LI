f905c55857502297f906df2de660c23540612231
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-04-09 23:31:24 -0300

    Add lean mode for table

--- src/cmds/test.ts
+++ src/cmds/test.ts
@@ -1,12 +1,32 @@
 import chalk from 'chalk';
 import cliTable from 'cli-table';
+import yargs from 'yargs';
 
 import testers from '../testers';
 import { hasTests, readProblemTests } from '../utils';
 
 export const command: string = 'test <testerOpt>';
 export const desc: string = 'Test code against samples';
-export const handler = ({ testerOpt = 'cpp' }: { testerOpt?: string }) => {
+export const builder = (yargs: yargs.Argv) => {
+  return yargs.options({
+    l: {
+      alias: 'lean',
+      boolean: true,
+      describe: 'Show lean table',
+      default: false,
+    },
+  });
+};
+
+const LEAN_TABLE_CONFIG = {
+  chars: { top: '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
+         , bottom: '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
+         , left: '' , 'left-mid': '' , mid: '' , 'mid-mid': ''
+         , right: '' , 'right-mid': '' , middle: ' ' },
+  style: { 'padding-left': 0, 'padding-right': 0 },
+};
+
+export const handler = ({ testerOpt = 'cpp', lean }: { testerOpt?: string, lean: boolean }) => {
   if (!hasTests()) {
     console.log('No tests in current directory');
     return;
@@ -16,15 +36,19 @@ export const handler = ({ testerOpt = 'cpp' }: { testerOpt?: string }) => {
   tester.beforeAll();
   const results = tests.map((test: ProblemTest, i) => tester.execute(`${i}`, test));
   tester.afterAll();
+  const head = [
+    'Test',
+    'Verdict',
+  ];
+  if (!lean) {
+    head.push('Input');
+    head.push('Output');
+    head.push('Expected output');
+  }
   const table = new cliTable(
     {
-      head: [
-        'Test',
-        'Verdict',
-        'Input',
-        'Output',
-        'Expected output',
-      ],
+      head: head.map(e => chalk.gray(e)),
+      ...(lean ? LEAN_TABLE_CONFIG : {}),
     },
   );
   results.forEach((result: ExecutionResult, i) => {
@@ -32,7 +56,13 @@ export const handler = ({ testerOpt = 'cpp' }: { testerOpt?: string }) => {
     if (result.expectedOutput.trim() !== result.output.trim()) {
       verdict = chalk.red('Incorrect');
     }
-    table.push([`#${i}`, verdict, result.input, result.output, result.expectedOutput])
-  })
-  console.log(table.toString())
+    const line = [`#${i}`, verdict];
+    if (!lean) {
+      line.push(result.input);
+      line.push(result.output);
+      line.push(result.expectedOutput);
+    }
+    table.push(line);
+  });
+  console.log(table.toString());
 };
