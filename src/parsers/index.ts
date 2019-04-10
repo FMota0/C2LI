b90f94b8525bd9f938d4ce3b6981ff8c0d0ac374
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-04-09 22:59:53 -0300

    Add codeforces gym parser

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -1,11 +1,12 @@
 import AtCoder from './AtCoder';
 import Codeforces from './Codeforces';
-
+import CodeforcesGym from './CodeforcesGym';
 import Parser from './Parser';
 
 const parsers: Record<string, Parser> = {
   atc: new AtCoder(),
   cf: new Codeforces(),
+  cfg: new CodeforcesGym(),
 };
 
 export default parsers;
