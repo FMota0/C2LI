8936887144407f6cc644575a538017466d4d6414
hugo galvao <hugo.galvao@ccc.ufcg.edu.br>
2019-04-12 19:15:58 -0300

    adding katting to parsers

--- src/parsers/index.ts
+++ src/parsers/index.ts
@@ -2,11 +2,13 @@ import AtCoder from './AtCoder';
 import Codeforces from './Codeforces';
 import CodeforcesGym from './CodeforcesGym';
 import Parser from './Parser';
+import Kattis from './Kattis';
 
 const parsers: Record<string, Parser> = {
   atc: new AtCoder(),
   cf: new Codeforces(),
   cfg: new CodeforcesGym(),
+  kt: new Kattis(),
 };
 
 export default parsers;
