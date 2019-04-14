743569fc8ca86bedc3e6114de415fd34b3f96e3b
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-04-13 22:40:08 -0300

    Add abstract parser

---
+++ src/testers/AbstractTester.ts
@@ -0,0 +1,30 @@
+import { spawnSync } from "child_process";
+
+abstract class AbstractTester implements Tester {
+  public abstract beforeAll: () => void;
+  public abstract afterAll: () => void;
+  public execute(id: string, test: ProblemTest): ExecutionResult {
+    const start = new Date().getTime();
+    const { command, args } = this.getExecutionCommand();
+    const result = spawnSync(command, args, {
+      input: test.input,
+      timeout: 1000,
+    });
+    const end = new Date().getTime();
+    const executionTime = (end - start)/1000;
+    if (result.error) {
+      console.log(result.error);
+      process.exit(0);
+    }
+    return {
+      input: test.input,
+      expectedOutput: test.output,
+      output: result.stdout.toString(),
+      timedOut: false,
+      executionTime,
+    }
+  };
+  public abstract getExecutionCommand: () => ExecutionCommand;
+}
+
+export default AbstractTester
