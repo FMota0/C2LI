1826e1a978e8a2e751a33c7dbd3d315487f98835
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-05-29 23:42:36 -0300

    Add remove command

---
+++ src/cmds/remove.ts
@@ -0,0 +1,33 @@
+import chalk from 'chalk';
+import yargs from 'yargs';
+
+import { readProblemTests, writeNewProblemsTest } from '../utils';
+
+export const command: string = 'remove <caseId>';
+
+export const desc: string = 'Remove a test case';
+
+interface RemoveArgs {
+  caseId: number;
+};
+
+export const builder = (yargs: yargs.Argv) => {
+  return yargs
+          .positional('caseId', {
+            type: 'number',
+          });
+};
+
+export const handler = (
+  {
+    caseId,
+  }: RemoveArgs
+) => {
+  let problemTests: ProblemTests = readProblemTests();
+  if (caseId < 0 || caseId >= problemTests.length) {
+    console.log(chalk.red('INVALID CASE ID'));
+    return;
+  }
+  problemTests.splice(caseId, 1);
+  writeNewProblemsTest(problemTests);
+};
