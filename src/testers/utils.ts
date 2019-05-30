67abdc653635bf7d428352518e4eddd1bdf06431
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2019-05-30 15:06:39 -0300

    Lint

--- src/testers/utils.ts
+++ src/testers/utils.ts
@@ -1,10 +1,10 @@
 import fs from 'fs';
 
-import Testers from './index';
+import testers from '../testers';
 
 export const getTesterOption = (): string|undefined => {
   const files = fs.readdirSync('.');
-  const suffixs = Object.keys(Testers);
+  const suffixs = Object.keys(testers);
   return suffixs.find(suffix => !!files.find(file => file.endsWith(suffix)));
 };
 
