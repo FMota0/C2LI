526ab35c3e84177ce276cc49774aea35c903a4c1
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2020-03-14 18:38:08 -0300

    Add compile command

---
+++ src/cmds/compile.ts
@@ -0,0 +1,35 @@
+import chalk from 'chalk';
+import yargs from 'yargs';
+
+import testerBuilder from '../testers';
+import { getFileValidSuffix } from '../testers/utils';
+
+export const command: string = 'compile [source]';
+export const desc: string = 'Execute beforeAll tester scrips';
+
+export const builder = (yargs: yargs.Argv) => {
+  return yargs
+          .positional('source', {
+            describe: 'Source where before all should be executed',
+            type: 'string',
+          });
+};
+
+interface CompileArgs {
+  source: string;
+}
+
+export const handler = (
+  {
+    source,
+  }: CompileArgs,
+) => {
+  const suffix = getFileValidSuffix(source);
+  if (!suffix) {
+    console.log(chalk.red('Invalid source file type'));
+    return;
+  }
+  const tester = testerBuilder(suffix, source);
+  tester.beforeAll();
+  console.log(chalk.green(`Runnable: ${tester.bin}`));
+};
